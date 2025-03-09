228716be3dd2627144f7ecd0ac877a0729e82508
rigxter <188875106+rigxter@users.noreply.github.com>
2025-03-09 11:41:05 +0100

    add description data side window of a todo

--- src/InterfaceController.js
+++ src/InterfaceController.js
@@ -1,21 +1,97 @@
-function createTodo() {
-    const titleTodo = document.querySelector("#Todo-Title");
-    const descriptionTodo = document.querySelector("#Todo-Description");
-    const dueDateTodo = document.querySelector("#Todo-DueDate");
-    const priorityTodo = document.querySelector("#Todo-Priority");
+// import { TodoItem } from "./index.js";
+// import { Category } from "./index.js";
 
-    new TodoItem(titleTodo, descriptionTodo, dueDateTodo, priorityTodo);
+export function getInputDataTodo() {
+    const titleTodo = document.querySelector("#Todo-Title").value;
+    const descriptionTodo = document.querySelector("#Todo-Description").value;
+    const dueDateTodo = document.querySelector("#Todo-DueDate").value;
+    const priorityTodo = document.querySelector("#Todo-Priority").value;
+
+    return [titleTodo, descriptionTodo, dueDateTodo, priorityTodo];
 }
 
-const dialog = document.querySelector("dialog");
+export function createTodoinList(todoTitle, todoDescription, tododueDate, todoPriority) {
+    const list = document.querySelector("#listTodos-container");
+    const divTodoCard = document.createElement("div");
+    const h4TodoCardTitle = document.createElement("h4");
+
+    divTodoCard.setAttribute("class", "listTodo");
+
+    divTodoCard.setAttribute("data-Title", todoTitle);
+    divTodoCard.setAttribute("data-description", todoDescription);
+    divTodoCard.setAttribute("data-dueDate", tododueDate);
+    divTodoCard.setAttribute("data-Priority", todoPriority);
+
+    h4TodoCardTitle.textContent = todoTitle;
+
+    list.append(divTodoCard);
+    divTodoCard.append(h4TodoCardTitle);
+}
+
+export function deleteTodoinList() {
+    const list = document.querySelector("#listTodos-container");
+    list.textContent = "";
+}
+
+export function addDeteilsWindowforTodo() {
+    const listTodoClass = document.querySelectorAll(".listTodo");
+    listTodoClass.forEach((element) => {
+        element.addEventListener("click", () => {
+            const todoTitle = element.getAttribute("data-title");
+            const todoDescription = element.getAttribute("data-description");
+            const todoCatagory = element.getAttribute("data-Catagory");
+            const todoPriority = element.getAttribute("data-Priority");
+            const todoDueDate = element.getAttribute("data-duedate");
+
+            const h4 = document.createElement("h4");
+            const p = document.createElement("p");
+            const pTextCatagory = document.createElement("p");
+            const spanTextCatagory = document.createElement("span");
+
+            const pTextDueDate = document.createElement("p");
+            const spanTextDueDate = document.createElement("span");
+            const divContainerCategory = document.createElement("div");
+            const divContainerDueDate = document.createElement("div");
+
+            const divContainerPriority = document.createElement("div");
+            const pTextPriority = document.createElement("p");
+            const spanTextPriority = document.createElement("span");
+
+            h4.textContent = todoTitle;
+            p.textContent = todoDescription;
+
+            listTodosData.textContent = "";
+
+            divContainerCategory.setAttribute("id", "listTodosDataCategory");
+            pTextCatagory.textContent = "Category";
+            spanTextCatagory.textContent = todoCatagory;
+            divContainerCategory.append(pTextCatagory, spanTextCatagory);
+
+            divContainerDueDate.setAttribute("id", "listTodosDataDueDate");
+            pTextDueDate.textContent = "Due Date";
+            spanTextDueDate.textContent = todoDueDate;
+            divContainerDueDate.append(pTextDueDate, spanTextDueDate);
+
+            divContainerPriority.setAttribute("id", "listTodosDataDueDate");
+            pTextPriority.textContent = "Priority";
+            spanTextPriority.textContent = todoPriority;
+            divContainerPriority.append(pTextPriority, spanTextPriority);
+
+            listTodosData.append(
+                h4,
+                p,
+                divContainerCategory,
+                divContainerDueDate,
+                divContainerPriority
+            );
+        });
+    });
+}
+
+export const dialog = document.querySelector("dialog");
+export const createTodoButton = document.querySelector("#createTodo");
 const showButtonDialog = document.querySelector("#addNewTodo");
 const closeButtonDialog = document.querySelector("#closeTodoCreater");
-const createTodoButton = document.querySelector("#createTodo");
-
-createTodoButton.addEventListener("click", () => {
-    createTodo();
-    console.log(TodoItem);
-});
 
 showButtonDialog.addEventListener("click", () => {
     dialog.showModal();
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 import "./InterfaceController.js";
-class TodoItem {
+import "./styles.css";
+import { getInputDataTodo } from "./InterfaceController.js";
+import { dialog } from "./InterfaceController.js";
+import { createTodoButton } from "./InterfaceController.js";
+import { createTodoinList } from "./InterfaceController.js";
+import { deleteTodoinList } from "./InterfaceController.js";
+import { addDeteilsWindowforTodo } from "./InterfaceController.js";
+
+export class TodoItem {
     constructor(title, description, dueDate, priority) {
         this.title = title;
         this.description = description;
@@ -8,7 +16,7 @@ class TodoItem {
     }
 }
 
-class Category {
+export class Category {
     constructor(title) {
         this.title = title;
         this.items = new Array();
@@ -24,8 +32,47 @@ class Category {
 }
 
 const TodoItemEmial = new TodoItem("Read email", "Read the work email", 2025 - 4 - 3, "high");
-const TodoItemFiel = new TodoItem("Read email", "Read the work email", 2025 - 4 - 3, "high");
-const Default = new Category("Default");
-Default.addItem(TodoItemEmial);
-Default.addItem(TodoItemFiel);
-console.log(Default.getItems());
+const TodoItemFiel = new TodoItem(
+    "Reserch something",
+    "Reaserch that paper for school",
+    2025 - 4 - 3,
+    "high"
+);
+const TodoItemEmiall = new TodoItem(
+    "Do the homework and study",
+    "Do the math homework from the page 12 - 13, all excersies.",
+    2025 - 4 - 3,
+    "high"
+);
+const TodoItemFiell = new TodoItem(
+    "Go to the supermarket and buy milk",
+    "Go to the supermakret and buy two crates of milk tha are on discount.",
+    2025 - 4 - 3,
+    "high"
+);
+const AllTodos = new Category("Default");
+AllTodos.addItem(TodoItemEmial);
+AllTodos.addItem(TodoItemFiel);
+AllTodos.addItem(TodoItemEmiall);
+AllTodos.addItem(TodoItemFiell);
+console.log(AllTodos.getItems());
+
+createTodoButton.addEventListener("click", () => {
+    const inputDataArr = getInputDataTodo();
+    AllTodos.addItem(
+        new TodoItem(inputDataArr[0], inputDataArr[1], inputDataArr[2], inputDataArr[3])
+    );
+    createListFromCategory();
+    dialog.close();
+});
+
+function createListFromCategory() {
+    deleteTodoinList();
+
+    AllTodos.getItems().forEach((element) => {
+        createTodoinList(element.title, element.description, element.dueDate, element.priority);
+    });
+    addDeteilsWindowforTodo();
+}
+createListFromCategory();
+addDeteilsWindowforTodo();
